6f4a8a30e0d918416aef8a9a170669722af4ee2a
GnoLyrad <115759985+GnoLyrad@users.noreply.github.com>
2022-10-14 11:24:43 +0800

    basic structure for FOMC scrape

--- 1_Data Pipeline/text_data_pipeline.py
+++ 1_Data Pipeline/text_data_pipeline.py
@@ -3,4 +3,28 @@ class TweetDataGetter():
     def __init__(self):
         pass
 
-    #
+
+import urllib
+class FOMC():
+    def __init__(self):
+        self.site = _
+        self.yrs = {} #storing the individual years in dict
+        self.rbindData = ""
+        
+    def getData():
+        #go to site and get the links for each year available
+        #FOMCLinks = urllib.request()  
+
+        #using number of links to loop to the sites
+        #for i in FOMCLinks:
+            #self.yrs[i] = urllib.request( #new url +  i if possible) #storing in the dictionary
+        return self.yrs
+
+    def bindData():
+        #for i in self.yrs():
+            #bind the dictionary
+            #rbindData #df form
+        return self.rbindData
+
+
+
