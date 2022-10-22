a383d17d78b51b9d8f7008632e245ae33c14a1cc
GnoLyrad <115759985+GnoLyrad@users.noreply.github.com>
2022-10-22 19:42:14 +0800

    Update text_data_pipeline.py
    uploading the current year text

--- 1_Data Pipeline/text_data_pipeline.py
+++ 1_Data Pipeline/text_data_pipeline.py
@@ -21,6 +21,7 @@ class FOMC():
         response = request.urlopen(req)
         return re.findall(r"""<\s*a\s*href=["']([^=]+)["']""", response.read().decode("utf-8"))
         #need run else 404 error
+
     def getCurrentYr():
         #turns out this appears in a different site so adding a separate function
         url = self.mainSite + "/monetarypolicy/fomccalendars.htm"
@@ -31,7 +32,38 @@ class FOMC():
             date = re.findall('[0-9]+', os.path.basename(i))[0]
             if date[:4] == "2022":
                 curYr_dict[int(date[4:6])] = mainSite + "/" + i
+        return readCurPDF(curYr_dict)
+
+    def readCurPDF(curYr_dict):
+        #loop through all the url in curYr_Dict
+        for i in curYr_dict.items():
+            curYr_dict[i[0]] = parsePDFcurYr(i[1], i[0])
         return curYr_dict
+
+    def parsePDFcurYr(url, month):
+        rq = requests.get(url)
+        pdf = pdfplumber.open(BytesIO(rq.content))
+        pgs = ""
+        #skipping pg 1 since it always is the names of the parties involved 
+        #in retrospect, the name may affect outcome?
+        for i in range(2,len(pdf.pages)): #range(startPg, endPg) future update
+            page = pdf.pages[i]
+            #done because pdf is split in 2 halves, and to bypass merged words issue
+            left = page.crop((0, 0.1 * float(page.height), 0.5 * float(page.width), 1 * float(page.height)))
+            right = page.crop((0.5 * float(page.width), 0.1 * float(page.height), page.width, 1 * float(page.height)))
+            pgs += left.extract_text().replace("\n", " ") + " " + right.extract_text().replace("\n"," ")   
+
+        pgs = pgs.replace("- ","")
+        pgs = " ".join(pgs.split())
+
+        if month == 1 or month == 2: #2 due to scheduling differences
+            temp = []
+            pgs = pgs[pgs.find("AUTHORIZATION FOR DOMESTIC OPEN MARKET OPERATIONS"):]
+        else:
+            pgs = pgs[pgs.find("Developments in Financial Markets and Open Market Operations"):]
+        return pgs.rsplit('Voting for this action', 1)[0]
+        #returns a dict with month as key, text as value, url is removed
+
     def getAllYears():
         #go to site and get the links for each of the previous years available
         url = self.mainSite + "/publications/annual-report.htm"
@@ -50,7 +82,6 @@ class FOMC():
             if len(yr) == 2:
                 yr = "19" + yr if int(yr) > 94 else "20" + yr
             yrs[int(yr)] = {"site" : i}
-        #get the mths for the cur yr #previous yr need to parse the pdf directly #might add it in the loop above
         #using 2022 directly, change if necessary
         yrs[2022] = getCurrentYr()
 
@@ -61,8 +92,6 @@ class FOMC():
     def readOldPDF():
         pass
         #using self.years
-    def readCurPDF():
-        pass
 
     def bindData():
         #loop through the dict and bind into 1 df
