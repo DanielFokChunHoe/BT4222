8eeb2c9adf45f6626e85f02902cdd1cd32d8e306
GnoLyrad <115759985+GnoLyrad@users.noreply.github.com>
2022-10-23 16:14:35 +0800

    Update text_data_pipeline.py

--- 1_Data Pipeline/text_data_pipeline.py
+++ 1_Data Pipeline/text_data_pipeline.py
@@ -1,9 +1,10 @@
 from urllib import request
 import re
-import requests, PyPDF2
 from io import BytesIO
 import datetime
-import urllib.request
+import requests, pdfplumber
+from urllib import request
+
 
 class TweetDataGetter():
 
@@ -14,15 +15,16 @@ class FOMC():
     def __init__(self):
         self.mainSite = "https://www.federalreserve.gov"
         self.years = getAllYears #storing the individual years in dict
+        self.user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
         #self.rbindData = bindData 
-    def getLinks(url):
-        headers={'User-Agent':user_agent,} 
+    def getLinks(self,url):
+        headers={'User-Agent':self.user_agent,} 
         req = request.Request(url,None,headers) #The assembled request
         response = request.urlopen(req)
         return re.findall(r"""<\s*a\s*href=["']([^=]+)["']""", response.read().decode("utf-8"))
         #need run else 404 error
 
-    def getCurrentYr():
+    def getCurrentYr(self):
         #turns out this appears in a different site so adding a separate function
         url = self.mainSite + "/monetarypolicy/fomccalendars.htm"
         urllist = [i for i in getLinks(url) if ".pdf" and "files/fomcminutes" in i]
@@ -34,13 +36,13 @@ class FOMC():
                 curYr_dict[int(date[4:6])] = mainSite + "/" + i
         return readCurPDF(curYr_dict)
 
-    def readCurPDF(curYr_dict):
+    def readCurPDF(self,curYr_dict):
         #loop through all the url in curYr_Dict
         for i in curYr_dict.items():
             curYr_dict[i[0]] = parsePDFcurYr(i[1], i[0])
         return curYr_dict
 
-    def parsePDFcurYr(url, month):
+    def parsePDFcurYr(self,url, month):
         rq = requests.get(url)
         pdf = pdfplumber.open(BytesIO(rq.content))
         pgs = ""
@@ -64,7 +66,7 @@ class FOMC():
         return pgs.rsplit('Voting for this action', 1)[0]
         #returns a dict with month as key, text as value, url is removed
 
-    def getAllYears():
+    def getPrevYrs(self):
         #go to site and get the links for each of the previous years available
         url = self.mainSite + "/publications/annual-report.htm"
 
@@ -82,20 +84,23 @@ class FOMC():
             if len(yr) == 2:
                 yr = "19" + yr if int(yr) > 94 else "20" + yr
             yrs[int(yr)] = {"site" : i}
+        return yrs #will be #readPrevPDF(yrs) #once done
+
+    def readPrevPDF(self, curYr_dict):
+        pass
+    def parsePDFcurYr(self, url):
+        pass
+
+    def getAllYears(self):
+        #go to site and get the links for each of the previous years available
+        yrs = getPrevYrs()
         #using 2022 directly, change if necessary
         yrs[2022] = getCurrentYr()
 
         #prob need add a catch clause here if the fed release the report in december of the same yr or before the january meeting
-        #reports_sites = [i if #current year check# in for i in reports_sites[0]]
         return yrs
 
-    def readOldPDF():
-        pass
-        #using self.years
 
-    def bindData():
+    def bindData(self):
         #loop through the dict and bind into 1 df
         return self.rbindData
-
-
-
